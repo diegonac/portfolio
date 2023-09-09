374cde0420a6d37e66471e599052c9a0b27766ca
Diego Nacimiento <diegonacimiento18@gmail.com>
2023-09-08 23:45:21 -0300

    feat: added splash

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import Layout from "./components/Layout";
+import Splash from "./components/Splash";
 import "./App.css";
+import { useState } from "react";
 
 const App = () => {
-  return <Layout />;
+  const [loading, setLoading] = useState(false);
+  if(!loading) {
+    setTimeout(() => {
+      setLoading(true);
+    }, 3000)
+    return <Splash />
+  }
+  return <Layout />
 };
 
 export default App;
---
+++ src/components/Splash.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../assets/Splash.css";
+
+const Splash: React.FC = () => {
+  return <div className="container-splash"><div id="splash"></div> </div>;
+};
+
+export default Splash;
