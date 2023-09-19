1f89b2e1d8eb8e03a118e5ab32f168840f254136
Diego Nacimiento <diegonacimiento18@gmail.com>
2023-09-19 01:49:57 -0300

    feat: changed tags nav-bar

--- src/components/header/NavBar.tsx
+++ src/components/header/NavBar.tsx
@@ -5,16 +5,45 @@ interface NavBarProps {
   handleDrawer?: () => void;
 }
 
-const NavBar: React.FC<NavBarProps> = ({ isMobile = false, handleDrawer = () => {} }) => {
+const NavBar: React.FC<NavBarProps> = ({ isMobile = false, handleDrawer = () => {}, }) => {
+  const display = isMobile ? undefined : { display: "flex" };
 
-  const display = isMobile ? undefined : {display: "flex"};
+  const updateScrollFocus = (id: string) => {
+    const container = document.getElementById(id);
+    const yOffset = -75;
+    if(container) {
+      const y = container.getBoundingClientRect().top + window.scrollY + yOffset;
+      window.scrollTo({ top: y, behavior: "smooth" });
+    }
+  };
+
+  const handlePresentation = () => {
+    handleDrawer();
+    updateScrollFocus("section-presentation");
+  }
+
+  const handleProject = () => {
+    handleDrawer();
+    updateScrollFocus("section-projects");
+  };
+
+  const handleContact = () => {
+    handleDrawer();
+    updateScrollFocus("section-contact");
+  };
 
   return (
     <nav id="nav-bar">
       <ul style={display}>
-        <li onClick={handleDrawer}><a href="#section-presentation">Sobre mí</a></li>
-        <li onClick={handleDrawer}><a href="#section-projects">Proyectos</a></li>
-        <li onClick={handleDrawer}><a href="#section-contact">Contacto</a></li>
+        <li onClick={handlePresentation}>
+          <p>Sobre mí</p>
+        </li>
+        <li onClick={handleProject}>
+          <p>Proyectos</p>
+        </li>
+        <li onClick={handleContact}>
+          <p>Contacto</p>
+        </li>
       </ul>
     </nav>
   );
