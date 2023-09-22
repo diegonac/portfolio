421d1b1658042dca2516b64269d930879d7e1155
Diego Nacimiento <diegonacimiento18@gmail.com>
2023-09-22 10:14:37 -0300

    feat: added import header, presentation css in splash component

--- src/components/Splash.tsx
+++ src/components/Splash.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "../assets/css/Splash.css";
+import "../assets/css/Header.css";
+import "../assets/css/Presentation.css";
 
 const Splash: React.FC = () => {
   return <div className="container-splash"><div id="splash"></div> </div>;
