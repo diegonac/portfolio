a90f53bea6ec528ffac59086f3f5a9625d65c03e
Diego Nacimiento <diegonacimiento18@gmail.com>
2024-09-11 23:43:00 -0300

    feat: update cv

--- src/utils/contactList.ts
+++ src/utils/contactList.ts
@@ -18,7 +18,7 @@ const contactList = [
     name: "CV",
     icon: "https://i.imgur.com/vyrRqm1.png",
     link: "https://www.dropbox.com/scl/fi/byc070iz13qjh3tjua198/CV_Full-Stack-Developer_Nacimiento-Diego.pdf?rlkey=mg72ebez8xknqsrnlhqnr4iya&dl=0",
-    linkSpanish: "https://www.dropbox.com/scl/fi/lh2ky1z9yi22y00rjcf7h/CV_Full-Stack-Developer_Nacimiento-Diego-Espa-ol.pdf?rlkey=fou8wsz45kvbuzydlbk4ohyle&dl=0"
+    linkSpanish: "https://www.dropbox.com/scl/fi/qb5d95vlwmi910lo32oas/CV_Diego-Nacimiento_Full-Stack-Developer.pdf?rlkey=z8yjc90v5hoddj5ouwvtnpl7w&st=ft8lrwzt&dl=0"
   },
 ];
 
