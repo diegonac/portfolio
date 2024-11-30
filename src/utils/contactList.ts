21d1e87e4a93fc84251adee50dd020424329a8b3
Diego Nacimiento <diegonacimiento18@gmail.com>
2024-11-30 18:02:36 -0300

    fix: dependencies and update cv link

--- src/utils/contactList.ts
+++ src/utils/contactList.ts
@@ -18,7 +18,7 @@ const contactList = [
     name: "CV",
     icon: "https://i.imgur.com/vyrRqm1.png",
     link: "https://www.dropbox.com/scl/fi/byc070iz13qjh3tjua198/CV_Full-Stack-Developer_Nacimiento-Diego.pdf?rlkey=mg72ebez8xknqsrnlhqnr4iya&dl=0",
-    linkSpanish: "https://www.dropbox.com/scl/fi/qb5d95vlwmi910lo32oas/CV_Diego-Nacimiento_Full-Stack-Developer.pdf?rlkey=z8yjc90v5hoddj5ouwvtnpl7w&st=ft8lrwzt&dl=0"
+    linkSpanish: "https://www.dropbox.com/scl/fi/i9rqteujfkelj09unjajp/CV_Full-Stack_Diego-Nacimiento.pdf?rlkey=jnireauayvi5z0jwrti3wdj0r&st=9rzn2qcq&raw=1"
   },
 ];
 
